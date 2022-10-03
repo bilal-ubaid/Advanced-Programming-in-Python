356228a3bc9fe07465d31c0aa277725c0f38a633
bilal-ubaid <113444003+bilal-ubaid@users.noreply.github.com>
2022-10-03 16:23:21 +0500

    Add files via upload

---
+++ Assignment 01/maze.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 15:54:37 2022
+
+@author: billu
+"""
+
+a = input("Where to go ?")
+while a == "straight":
+    print("going forward!")
+    a = input("Where to go ?") 
+while a == "left":
+    print("Going left!")
+    a = input("Where to go ?")
+while a == "right":
+    print("Going right!")
+    a = input("Where to go ?")
